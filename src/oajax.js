1afdb589d20587dfd702291efe52c034f0a933fb
Ali <ali@opendynamics.com.my>
2017-10-11 17:42:01 +0800

    Update oajax

--- src/oajax.js
+++ src/oajax.js
@@ -1,64 +1,100 @@
+var ref_cache = '';
+var ref_request = '';
 
-var oajax ={
+var $off_ajax = $.ajax;
+
+(function($){
+
+var $oajax = $.ajax;
+
+
+$.ajax = function(o){
 	
-  logMsg : 0,
-  
-  request : function( Type , URL  , Data , ContentType ,callback){
-  
-    $.ajax({
-          
-          type: Type,
-          url: URL,
-		  data:  JSON.stringify(Data) ,
-		  contentType: ContentType ,
-         
-          success: function(data){
-              window.localStorage.setItem("cache@" + URL + "%" +JSON.stringify(Data) , JSON.stringify(data) );
-              
-			  if(oajax.logMsg)
-			  console.log("getting data online");
-             
-			  callback(data);
-          
-        },
-          error: function (request, status, error) {
-           
-		    
-			 
-			 if (localStorage.getItem("cache@" + URL + "%" +JSON.stringify(Data)) === null) {
-            
-				var handler = "request@" + URL + "%" +JSON.stringify(Data);
+	
+	
+	if(o.data){
+		var Data = JSON.stringify(o.data) ;
+		ref_cache = "cache@" + o.url + "%" + Data ;
+		ref_request = "request@" + o.url + "%" + Data ;
+	}
+	else {
+		ref_cache = "cache@" + o.url ;
+		ref_request = "request@" + o.url ;
+	}
+	
+	
+	
+     if (o.error) {
+		var originalErrorHandler2 = o.success;
+		var errorHandlerContext2 = o.context ? o.context : $;
+		var customError2Handler = function(result) {
+		    result = JSON.parse("{ \"error\" : 404 }");
 			
-				if(localStorage.getItem(handler) === null){
-					var Request = {
-									type: Type,
-									url: URL,
-									data:  JSON.stringify(Data),
-									contentType: ContentType 
-								  };
-								 
-					
-					window.localStorage.setItem(handler , JSON.stringify(Request));
-					sync_que =  oajax.getQue();
-					sync_que.push(handler);
-					window.localStorage.setItem("sync_que" , JSON.stringify(sync_que));
-					callback("{\"error\" : \"404\"}");
+		if (localStorage.getItem(ref_cache) === null) {
+            if(localStorage.getItem(ref_request) === null){
+				var ajaxState = JSON.stringify(o);
+				window.localStorage.setItem(ref_request , ajaxState);
+				sync_que =  oajax.getQue();
+				sync_que.push(ref_request);
+				window.localStorage.setItem("sync_que" , JSON.stringify(sync_que));
+			}
+		}
+        
+		else{
+				 
+		    if(oajax.logMsg)
+                console.log("getting data offline");
+                
+			var data = localStorage.getItem(ref_cache);
+				
+				if(oajax.parseResult){
+					result = JSON.parse(data);
 				}
 				else{
-					callback("{\"error\" : \"404\"}");
+					result = data;
 				}
 				
-             }
-             else{
-				 
-				 if(oajax.logMsg)
-                console.log("getting data offline");
-                callback(localStorage.getItem("cache@" + URL + "%" + JSON.stringify(Data)));
-             }
-          }
-      }); 
-  },
+        }
+			
+			
+			originalErrorHandler2.apply(errorHandlerContext2, arguments);
+        
+        };
 
+        // override error callback with custom implementation           
+        o.error = customError2Handler;
+    };
+	
+	
+	if(o.success){
+		var originalErrorHandler = o.success;
+		var errorHandlerContext = o.context ? o.context : $;
+        var customErrorHandler = function(result) {
+			window.localStorage.setItem(ref_cache , JSON.stringify(result) );
+             if(oajax.logMsg)
+			  console.log("getting data online");
+            
+			originalErrorHandler.apply(errorHandlerContext, arguments);
+        };
+		o.success = customErrorHandler;
+	}
+	
+
+    $oajax.apply($, arguments);
+};
+
+})(jQuery);
+
+var $oajax = $.ajax;
+
+
+
+
+
+var oajax ={
+   
+  parseResult : true,
+  logMsg :true ,
   
   getQue : function(){
     if (localStorage.getItem("sync_que") === null) {
@@ -79,9 +115,11 @@ var oajax ={
 		for(; i < sync_que.length ; ++i ){
          
          oajax.syncData(sync_que[i], function(e){
-           if(e=="success"){
-			 sync_que.splice(i, 1);
-		   }
+			 console.log(e);
+			if(e=="success"){
+			 sync_que.splice(i , 1);
+			 //localStorage.setItem("sync_que" , JSON.stringify(sync_que));
+			}
 		   
            
          });
@@ -96,30 +134,34 @@ var oajax ={
     
 	var Request = JSON.parse(localStorage.getItem(handler));
 	
+	if(Request.data){
+		var Data = JSON.stringify(Request.data) ;
+		ref_cache = "cache@" + Request.url + "%" + Data ;
+	}
+	else {
+		ref_cache = "cache@" + Request.url ;
+	}
 	
-	$.ajax({
-			
-			type: Request.type ,
-			async: false,
-			url: Request.url ,
-			data: Request.data ,
-			contentType: Request.contentType ,
-			
-            
-			success: function(data){
+	
+	
+	Request.async = false;
+	Request.success = function(data){
 				
-				 window.localStorage.setItem("cache@" +  Request.url + "%" + Request.data, JSON.stringify(data) );
+				 window.localStorage.setItem(ref_cache, JSON.stringify(data) );
 				
                  if(oajax.logMsg)
 				console.log(handler +": "+ Request.url + " sync with server");
                 localStorage.removeItem(handler);
 				callback("success");
-            },
-            error: function (request, status, error){
-                callback("error");
-            }
-       
-       });
+            };
+	Request.error = function (request, status, error){
+				 callback("error");
+            };
+	
+	
+	
+	
+	$off_ajax(Request);
   
   }
 
